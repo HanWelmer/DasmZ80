491bd8643a476eaacf69e3af87a0292d6dc64d9e
Han Welmer <han.welmer@tno.nl>
2024-10-16 16:16:56 +0200

    Remove attribute nrOfBytes from BinaryCode.

--- src/main/java/com/github/hanwelmer/dasmZ80/BinaryCode.java
+++ src/main/java/com/github/hanwelmer/dasmZ80/BinaryCode.java
@@ -3,24 +3,18 @@ package com.github.hanwelmer.dasmZ80;
 public class BinaryCode {
 
   private int code;
-  private int nrOfBytes;
   private String mask;
   private String mnemonic;
 
   // Constructor.
-  public BinaryCode(int code, int nrOfBytes, String mask, String mnemonic) {
+  public BinaryCode(int code, String mask, String mnemonic) {
 	this.code = code;
-	this.nrOfBytes = nrOfBytes;
 	this.mask = mask;
 	this.mnemonic = mnemonic;
   }
 
   public int getCode() {
 	return code;
-  }
-
-  public int getNrOfBytes() {
-	return nrOfBytes;
   }
 
   public String getMask() {
--- src/main/java/com/github/hanwelmer/dasmZ80/Decoder.java
+++ src/main/java/com/github/hanwelmer/dasmZ80/Decoder.java
@@ -96,7 +96,7 @@ public class Decoder {
 	if (asmCode.getMnemonic().contains("%")) {
 	  Byte byte2 = reader.getNextByte();
 	  asmCode.addByte(byte2);
-	  int targetAddress = address + binCode.getNrOfBytes();
+	  int targetAddress = address + asmCode.getBytes().size();
 	  targetAddress += (byte2 < 128) ? byte2 : (-byte2);
 	  asmCode.updateMnemonic("%", String.format("lbl%04X", targetAddress));
 	}
@@ -121,33 +121,33 @@ public class Decoder {
 
   // Constructor
   public Decoder() {
-	hashMap.put(new Integer(0x00), new BinaryCode(0x00, 1, "00", "NOP"));
-	hashMap.put(new Integer(0x01), new BinaryCode(0x01, 3, "01@@@@", "LD   BC,@"));
-	hashMap.put(new Integer(0x02), new BinaryCode(0x02, 1, "02", "LD   (BC),A"));
-	hashMap.put(new Integer(0x06), new BinaryCode(0x06, 2, "06##", "LD   B,#"));
-	hashMap.put(new Integer(0x10), new BinaryCode(0x10, 2, "10%%", "DJNZ %"));
-	hashMap.put(new Integer(0x76), new BinaryCode(0x76, 1, "76", "HALT"));
-	hashMap.put(new Integer(0xD3), new BinaryCode(0xD3, 2, "D3&&", "OUT  (&),A"));
-	hashMap.put(new Integer(0xCB), new BinaryCode(0xCB, 2, "CB", "<next>"));
-	hashMap.put(new Integer(0xDD), new BinaryCode(0xDD, 2, "DD", "<next>"));
-	hashMap.put(new Integer(0xED), new BinaryCode(0xED, 2, "ED", "<next>"));
-	hashMap.put(new Integer(0xFD), new BinaryCode(0xFD, 2, "FD", "<next>"));
-	hashMap.put(new Integer(0xCB00), new BinaryCode(0xCB00, 2, "CB00", "RLC  B"));
-	hashMap.put(new Integer(0xDD19), new BinaryCode(0xDD19, 2, "DD19", "ADD  IX,DE"));
-	hashMap.put(new Integer(0xDD21), new BinaryCode(0xDD21, 4, "DD21@@@@", "LD   IX,@"));
-	hashMap.put(new Integer(0xDD26), new BinaryCode(0xDD26, 3, "DD26##", "LD   IXH,#"));
-	hashMap.put(new Integer(0xDD34), new BinaryCode(0xDD34, 3, "DD34$$", "INC  (IX+$)"));
-	hashMap.put(new Integer(0xDD36), new BinaryCode(0xDD36, 4, "DD36$$##", "LD   (IX+$),#"));
-	hashMap.put(new Integer(0xED40), new BinaryCode(0xED40, 2, "ED40", "IN   B,(C)"));
-	hashMap.put(new Integer(0xED43), new BinaryCode(0xED43, 4, "ED43@@@@", "LD   (@),BC"));
-	hashMap.put(new Integer(0xFD26), new BinaryCode(0xFD26, 3, "FD26##", "LD   IYH,#"));
-	hashMap.put(new Integer(0xFD35), new BinaryCode(0xFD35, 3, "FD35$$", "DEC  (IY+$)"));
-	hashMap.put(new Integer(0xFD36), new BinaryCode(0xFD36, 4, "FD36$$##", "LD   (IY+$),#"));
-	hashMap.put(new Integer(0xDDCB), new BinaryCode(0xDDCB, 4, "DDCB", "<skip,next>"));
-	hashMap.put(new Integer(0xFDCB), new BinaryCode(0xFDCB, 4, "FDCB", "<skip,next>"));
-	hashMap.put(new Integer(0xDDCB0000), new BinaryCode(0xDDCB0000, 4, "DDCB$$00", "LD   B,RLC (IX+$)"));
-	hashMap.put(new Integer(0xFDCB0001), new BinaryCode(0xFDCB0001, 4, "FDCB$$01", "LD   C,RLC (IY+$)"));
-	hashMap.put(new Integer(0xFDCB0080), new BinaryCode(0xFDCB0080, 4, "FDCB$$80", "LD   B,RES 0,(IY+$)"));
+	hashMap.put(new Integer(0x00), new BinaryCode(0x00, "00", "NOP"));
+	hashMap.put(new Integer(0x01), new BinaryCode(0x01, "01@@@@", "LD   BC,@"));
+	hashMap.put(new Integer(0x02), new BinaryCode(0x02, "02", "LD   (BC),A"));
+	hashMap.put(new Integer(0x06), new BinaryCode(0x06, "06##", "LD   B,#"));
+	hashMap.put(new Integer(0x10), new BinaryCode(0x10, "10%%", "DJNZ %"));
+	hashMap.put(new Integer(0x76), new BinaryCode(0x76, "76", "HALT"));
+	hashMap.put(new Integer(0xD3), new BinaryCode(0xD3, "D3&&", "OUT  (&),A"));
+	hashMap.put(new Integer(0xCB), new BinaryCode(0xCB, "CB", "<next>"));
+	hashMap.put(new Integer(0xDD), new BinaryCode(0xDD, "DD", "<next>"));
+	hashMap.put(new Integer(0xED), new BinaryCode(0xED, "ED", "<next>"));
+	hashMap.put(new Integer(0xFD), new BinaryCode(0xFD, "FD", "<next>"));
+	hashMap.put(new Integer(0xCB00), new BinaryCode(0xCB00, "CB00", "RLC  B"));
+	hashMap.put(new Integer(0xDD19), new BinaryCode(0xDD19, "DD19", "ADD  IX,DE"));
+	hashMap.put(new Integer(0xDD21), new BinaryCode(0xDD21, "DD21@@@@", "LD   IX,@"));
+	hashMap.put(new Integer(0xDD26), new BinaryCode(0xDD26, "DD26##", "LD   IXH,#"));
+	hashMap.put(new Integer(0xDD34), new BinaryCode(0xDD34, "DD34$$", "INC  (IX+$)"));
+	hashMap.put(new Integer(0xDD36), new BinaryCode(0xDD36, "DD36$$##", "LD   (IX+$),#"));
+	hashMap.put(new Integer(0xED40), new BinaryCode(0xED40, "ED40", "IN   B,(C)"));
+	hashMap.put(new Integer(0xED43), new BinaryCode(0xED43, "ED43@@@@", "LD   (@),BC"));
+	hashMap.put(new Integer(0xFD26), new BinaryCode(0xFD26, "FD26##", "LD   IYH,#"));
+	hashMap.put(new Integer(0xFD35), new BinaryCode(0xFD35, "FD35$$", "DEC  (IY+$)"));
+	hashMap.put(new Integer(0xFD36), new BinaryCode(0xFD36, "FD36$$##", "LD   (IY+$),#"));
+	hashMap.put(new Integer(0xDDCB), new BinaryCode(0xDDCB, "DDCB", "<skip,next>"));
+	hashMap.put(new Integer(0xFDCB), new BinaryCode(0xFDCB, "FDCB", "<skip,next>"));
+	hashMap.put(new Integer(0xDDCB0000), new BinaryCode(0xDDCB0000, "DDCB$$00", "LD   B,RLC (IX+$)"));
+	hashMap.put(new Integer(0xFDCB0001), new BinaryCode(0xFDCB0001, "FDCB$$01", "LD   C,RLC (IY+$)"));
+	hashMap.put(new Integer(0xFDCB0080), new BinaryCode(0xFDCB0080, "FDCB$$80", "LD   B,RES 0,(IY+$)"));
   }
 
 }
